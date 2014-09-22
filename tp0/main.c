e25d637202ebdcefab459b9938ca0b2c8a1fd00d
danielap.riesgo <danielap.riesgo@gmail.com>
2014-09-22 22:03:40 +0000

    Agregué lo de la escala del plano, pero NO COMPILÉ !

--- tp0/main.c
+++ tp0/main.c
@@ -29,14 +29,20 @@ void generatePGM(OutputData* data){
     pgm_image.col = data->resolution[1];
     pgm_image.max_gray = 256;
     pgm_image.matrix = allocate_dynamic_matrix(pgm_image.row, pgm_image.col);
-
+    
+    double first_real_value = - ((float) data->plane[0])/2;
+    double first_imaginary_value = ((float)data->plane[1]/2;
+    double width_scale = ((float) data->plane[0]) / data->resolution[0];
+    double height_scale =  - ((float) data->plane[1]) / data->resolution[1];
+	
     for(int i = 0; i < pgm_image.row; i++){
         for(int j = 0; j < pgm_image.col; j++){
-            pixel_t* pixel = crear_pixel(i, j);
+            pixel_t* pixel = crear_pixel(first_real_value + i * width_scale, first_imaginary_value + j * height_scale);
             pgm_image.matrix[i][j] = velocidad_de_escape(pixel);
             destruir_pixel(pixel);
         }
     }
+    
     writePGM(data->output, &pgm_image);
     deallocate_dynamic_matrix(pgm_image.matrix, pgm_image.row);
 }
