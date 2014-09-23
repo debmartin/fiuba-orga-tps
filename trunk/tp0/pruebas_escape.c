dc5c76cca0555416061f00239cc51e104811abf0
danielap.riesgo@gmail.com <danielap.riesgo@gmail.com@5d844513-12bd-12c2-3829-b3425b3846b4>
2014-09-23 05:32:41 +0000

    Más pruebas no extremas

--- trunk/tp0/pruebas_escape.c
+++ trunk/tp0/pruebas_escape.c
@@ -10,4 +10,15 @@ int main(){
 	int vel2 = velocidad_de_escape(pixel2);
 	printf("%s: %s\n", "Vel de escape de 10+0i es 0", (vel2 == 0)? "OK" : "ERROR");
 	
+   pixel_t* pixel3 = crear_pixel(-1, 0);
+	int vel3 = velocidad_de_escape(pixel3);
+	printf("%s: %s\n", "Vel de escape de -1+0i es 255", (vel3 == 255)? "OK" : "ERROR");
+	
+	pixel_t* pixel4 = crear_pixel(0.5, 0);
+	int vel4 = velocidad_de_escape(pixel4);
+	printf("%s: %s\n", "Vel de escape de 0,5+0i es 4", (vel4 == 4)? "OK" : "ERROR");
+	
+	pixel_t* pixel5 = crear_pixel(1, 0);
+	int vel5 = velocidad_de_escape(pixel5);
+	printf("%s: %s\n", "Vel de escape de 1+0i es 2", (vel5 == 2)? "OK" : "ERROR");
 }
