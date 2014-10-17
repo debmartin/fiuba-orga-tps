2dc820633fcffe32bb61bb6469f0715f90c83c68
danielap.riesgo@gmail.com <danielap.riesgo@gmail.com@5d844513-12bd-12c2-3829-b3425b3846b4>
2014-10-17 04:06:58 +0000

    Correcciones

--- trunk/tp0/pruebas_escape2.c
+++ trunk/tp0/pruebas_escape2.c
@@ -1,61 +1,70 @@
 #include <stdio.h>
+#define max_gray 255
 
 int main(){
-        double real, imag;
+        double real, imag, real2, imag2;
         int vel;
-        
+       
         real = 0;
         imag = 0;
-        
+        real2 = real;
+        imag2 = imag;
+       
         for (vel = 0; vel < max_gray; ++vel) {
-			if ((real * real + imag * imag) > 4)
+            if ((real2 * real2 + imag2 * imag2) > 4)
             //Módulo al cuadrado > 4
 				break;
             // Sino, elevo al cuadrado y le sumo a si mismo
-            real = real * real - imag * imag + real;
-            imag = 2 * real * imag + imag;
+            real2 = real2 * real2 - imag2 * imag2 + real;
+            imag2 = 2 * real2 * imag2 + imag;
         }
-        
+       
         printf("%s: %s\n", "Vel de escape de 0+0i es 255", (vel == 255)? "OK" : "ERROR");
 
         real = 10;
         imag = 0;
-        
+        real2 = real;
+        imag2 = imag;
+       
         for (vel = 0; vel < max_gray; ++vel) {
-			if ((real * real + imag * imag) > 4)
+                        if ((real2 * real2 + imag2 * imag2) > 4)
             //Módulo al cuadrado > 4
-				break;
+                                break;
             // Sino, elevo al cuadrado y le sumo a si mismo
-            real = real * real - imag * imag + real;
-            imag = 2 * real * imag + imag;
+            real2 = real2 * real2 - imag2 * imag2 + real;
+            imag2 = 2 * real2 * imag2 + imag;
         }
 
         printf("%s: %s\n", "Vel de escape de 10+0i es 0", (vel == 0)? "OK" : "ERROR");
        
         real = -1;
         imag = 0;
-        
+        real2 = real;
+        imag2 = imag;
+       
         for (vel = 0; vel < max_gray; ++vel) {
-			if ((real * real + imag * imag) > 4)
+                        if ((real2 * real2 + imag2 * imag2) > 4)
             //Módulo al cuadrado > 4
-				break;
+                                break;
             // Sino, elevo al cuadrado y le sumo a si mismo
-            real = real * real - imag * imag + real;
-            imag = 2 * real * imag + imag;
+            real2 = real2 * real2 - imag2 * imag2 + real;
+            imag2 = 2 * real2 * imag2 + imag;
         }
 
         printf("%s: %s\n", "Vel de escape de -1+0i es 255", (vel == 255)? "OK" : "ERROR");
        
         real = 0.5;
         imag = 0;
-        
+        real2 = real;
+        imag2 = imag;
+       
         for (vel = 0; vel < max_gray; ++vel) {
-			if ((real * real + imag * imag) > 4)
+                        if ((real2 * real2 + imag2 * imag2) > 4)
             //Módulo al cuadrado > 4
-				break;
+                                break;
             // Sino, elevo al cuadrado y le sumo a si mismo
-            real = real * real - imag * imag + real;
-            imag = 2 * real * imag + imag;
+            real2 = real2 * real2 - imag2 * imag2 + real;
+            imag2 = 2 * real2 * imag2 + imag;
         }
 
 
@@ -63,14 +72,16 @@ int main(){
        
         real = 1;
         imag = 0;
-        
+        real2 = real;
+        imag2 = imag;
+       
         for (vel = 0; vel < max_gray; ++vel) {
-			if ((real * real + imag * imag) > 4)
+                        if ((real2 * real2 + imag2 * imag2) > 4)
             //Módulo al cuadrado > 4
-				break;
+                                break;
             // Sino, elevo al cuadrado y le sumo a si mismo
-            real = real * real - imag * imag + real;
-            imag = 2 * real * imag + imag;
+            real2 = real2 * real2 - imag2 * imag2 + real;
+            imag2 = 2 * real2 * imag2 + imag;
         }
 
 
