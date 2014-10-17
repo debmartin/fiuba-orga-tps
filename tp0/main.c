1ddde6b68b7ccc98807f1772c6ff7e6527f64228
debbie1new.world <debbie1new.world@5d844513-12bd-12c2-3829-b3425b3846b4>
2014-10-17 18:18:50 +0000

    main junto salvo quitar complejos

--- tp0/main.c
+++ tp0/main.c
@@ -4,7 +4,6 @@
 #include <stdbool.h>
 #include <string.h>
 
-#include "pgm.h"
 #include "velocidad_escape.h"
 
 #define DEFAULT_RESOLUTION_WIDTH 640
@@ -14,6 +13,7 @@
 #define DEFAULT_PLANE_WIDTH 4
 #define DEFAULT_PLANE_HEIGHT 4
 #define ARG_DEFAULT_OUT "-"
+#define N 256
 
 
 typedef struct _OutputData{
@@ -22,47 +22,6 @@ typedef struct _OutputData{
     float plane[2];
     FILE* output;
 } OutputData;
-
-void generatePGM(OutputData* data){
-    PGMData pgm_image;
-    pgm_image.row = data->resolution[1];
-    pgm_image.col = data->resolution[0];
-    pgm_image.max_gray = 255;
-    pgm_image.matrix = allocate_dynamic_matrix(pgm_image.row, pgm_image.col);
-
-    double first_real_value = data->center[0] - data->plane[0]/2;
-    double first_imaginary_value = data->center[1] + data->plane[1]/2;
-    double width_scale = (data->plane[0] / data->resolution[0]);
-    double height_scale =  - (data->plane[1] / data->resolution[1]);
-    first_real_value += width_scale/2;
-    first_imaginary_value += height_scale/2;
-
-    for(int i = 0; i < pgm_image.row; i++){
-        for(int j = 0; j < pgm_image.col; j++){
-            complejo* num = crear_complejo(first_real_value + j * width_scale, first_imaginary_value + i * height_scale);
-            pgm_image.matrix[i][j] = velocidad_de_escape(num);
-            destruir_complejo(num);
-        }
-    }
-
-    writePGM(data->output, &pgm_image);
-    deallocate_dynamic_matrix(pgm_image.matrix, pgm_image.row);
-}
-
-void OutputDataInitialize(OutputData* data){
-    data->resolution[0] = DEFAULT_RESOLUTION_WIDTH;
-    data->resolution[1] = DEFAULT_RESOLUTION_HEIGHT;
-    data->center[0] = DEFAULT_CENTER_REAL;
-    data->center[1] =  DEFAULT_CENTER_IMAG;
-    data->plane[0] = DEFAULT_PLANE_WIDTH;
-    data->plane[1] = DEFAULT_PLANE_HEIGHT;
-    data->output = stdout;
-}
-
-int terminateError(char* errorMessaje){
-    perror(errorMessaje);
-    return 1;
-}
 
 int main(int argc, char* argv[]){
     static struct option long_options[] =
@@ -76,7 +35,14 @@ int main(int argc, char* argv[]){
     };
 
     OutputData data;
-    OutputDataInitialize(&data);
+    data.resolution[0] = DEFAULT_RESOLUTION_WIDTH;
+    data.resolution[1] = DEFAULT_RESOLUTION_HEIGHT;
+    data.center[0] = DEFAULT_CENTER_REAL;
+    data.center[1] =  DEFAULT_CENTER_IMAG;
+    data.plane[0] = DEFAULT_PLANE_WIDTH;
+    data.plane[1] = DEFAULT_PLANE_HEIGHT;
+    data.output = stdout;
+
     bool need_close = false;
     char option, i;
     int option_index;
@@ -128,7 +94,44 @@ int main(int argc, char* argv[]){
                 return 1;
         }
     }
-    generatePGM(&data);
+
+    fprintf(data.output, "P2\n");
+	fprintf(data.output, "%d\n%d\n", data.resolution[0], data.resolution[1]);
+	fprintf(data.output, "%d\n", 255);
+
+    double first_real_value = data.center[0] - data.plane[0]/2;
+    double first_imaginary_value = data.center[1] + data.plane[1]/2;
+    double width_scale = (data.plane[0] / data.resolution[0]);
+    double height_scale =  - (data.plane[1] / data.resolution[1]);
+    first_real_value += width_scale/2;
+    first_imaginary_value += height_scale/2;
+
+    int matrix[data.resolution[1]][data.resolution[0]];
+
+    for(int i = data.resolution[1]-1; i >= 0; i--){
+        for(int j = 0; j < data.resolution[0]; j++){
+            double real1 = first_real_value + j * width_scale, imag1 = first_imaginary_value + i * height_scale;
+            double real2 = real1, imag2 = imag1;
+            complejo* num1 = crear_complejo(first_real_value + j * width_scale, first_imaginary_value + i * height_scale);
+            complejo* num2 = crear_complejo(first_real_value + j * width_scale, first_imaginary_value + i * height_scale);
+
+            int vel;
+            for (vel = 0; vel < (N - 1); ++vel) {
+                if (((unsigned int) (real2*real2 + imag2*imag2)) > 4)
+                    break;
+                num2 = sumar(al_2(num2), num1);
+                real2 = getReal(num2);
+                imag2 = getImag(num2);
+//                real2 = real2 * real2 - imag2 * imag2 + real1;
+//                imag2 = 2 * real2 * imag2 + imag1;
+            }
+
+            fprintf(data.output, "%d ", vel);
+        }
+        fprintf(data.output, "\n");
+    }
+
+
     if (need_close) fclose(data.output);
     return 0;
 }
