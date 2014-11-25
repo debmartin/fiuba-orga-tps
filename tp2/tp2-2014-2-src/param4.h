75ea5b60f990a316d26f77382c72edb787ec0c03
danielap.riesgo <danielap.riesgo@gmail.com>
2014-11-25 08:10:54 +0000

    TP2

---
+++ tp2/tp2-2014-2-src/param4.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	float UL_re;
+	float UL_im;
+	float LR_re;
+	float LR_im;
+	float d_re;
+	float d_im;
+
+	size_t x_res;
+	size_t y_res;
+	size_t shades;
+
+	size_t y0;
+	size_t y1;
+	size_t x0;
+	size_t x1;
+
+	uint8_t* bitmap;
+} param_t;
